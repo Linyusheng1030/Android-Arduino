0dff0b51b331682941d4fd7353cb806bb1bd7b87
Linyusheng1030 <d04221082@gm.tut.edu.tw>
2018-09-17 12:04:52 +0800

    Initial commit

---
+++ app/src/main/java/tw/edu/tut/mis/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package tw.edu.tut.mis.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    private final String TAG="MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG,"bbbbbbbbbbbbb");
+    }
+}
